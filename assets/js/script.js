c5e437246f6231576f9c95445aa111d9a55164c7
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-11-06 21:33:29 -0500

    adds functions to handle form submission for search, and working search function using input from form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,34 @@
-var searchBtn = document.getElementById("search");
+var searchFormEl = document.getElementById("search-form");
+var cityInput = document.getElementById("city-search");
+var apiKey = "78d9b295a8a505d8adc473cd8e483c38";
 
-// function to test api
-function apiTest() {
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?lat=41.7637&lon=-72.6850&appid=78d9b295a8a505d8adc473cd8e483c38";
+function formSubmitHandler(event) {
+  event.preventDefault();
 
-  fetch(url)
+  var cityName = cityInput.value.trim();
+
+  if (cityName) {
+    search(cityName);
+    cityName.value = "";
+  } else {
+    alert("Enter a city name.");
+  }
+}
+
+function search(cityName) {
+  var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(weatherUrl)
     .then(function (response) {
+      console.log(response);
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+    })
+    .catch(function (error) {
+      console.error(error);
     });
 }
 
-searchBtn.addEventListener("click", apiTest);
+searchFormEl.addEventListener("submit", formSubmitHandler);
